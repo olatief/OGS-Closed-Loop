40cd714423d3251d12052289f68ecf14b04e8bab
olatief <olatief@gmail.com>
2011-06-20 23:49:38 -0400

    Added Scope Class

---
+++ CID_USB_BaseStation GUI/CID_USB_BaseStation/Scope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CID_USB_BaseStation
+{
+    class Scope
+    {
+
+        public Scope()
+        {
+        }
+    }
+}
